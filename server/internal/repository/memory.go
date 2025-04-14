3d41b7c0b1438d7d9a308340f6e0bb3215fad21a
vv-sam <vsamoilenko17@gmail.com>
2025-04-14 13:56:37 +0300

    concurrency

--- server/internal/repository/memory.go
+++ server/internal/repository/memory.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/vv-sam/otus-project/server/internal/model/agent"
 	"github.com/vv-sam/otus-project/server/internal/model/configuration"
@@ -10,14 +11,23 @@ import (
 )
 
 var (
+	m sync.RWMutex
+
 	agents  = make([]*agent.Info, 0)
 	configs = make([]*configuration.Minecraft, 0)
 	metric  = make([]*metrics.HostMetrics, 0)
 	tasks   = make([]*task.Task, 0)
+
+	agentsCount  = len(agents)
+	configsCount = len(configs)
+	metricCount  = len(metric)
+	tasksCount   = len(tasks)
 )
 
-func PassStructs(structs []fmt.Stringer) {
-	for _, s := range structs {
+func PassStructs(ch chan fmt.Stringer) {
+	for s := range ch {
+		m.Lock()
+
 		switch v := s.(type) {
 		case *agent.Info:
 			agents = append(agents, v)
@@ -28,25 +38,42 @@ func PassStructs(structs []fmt.Stringer) {
 		case *task.Task:
 			tasks = append(tasks, v)
 		}
+
+		m.Unlock()
 	}
 }
 
-func PrintValues() {
-	for _, s := range agents {
-		fmt.Println(s.String())
+func CheckUpdates() {
+	m.RLock()
+	defer m.RUnlock()
+
+	fmt.Println("Checking updates...")
+	if len(agents) != agentsCount {
+		for _, a := range agents[agentsCount:] {
+			fmt.Printf("Update: %q\n", a.String())
+		}
+		agentsCount = len(agents)
 	}
 
-	for _, s := range configs {
-		fmt.Println(s.String())
+	if len(configs) != configsCount {
+		for _, c := range configs[configsCount:] {
+			fmt.Printf("Update: %q\n", c.String())
+		}
+		configsCount = len(configs)
 	}
 
-	for _, s := range metric {
-		fmt.Println(s.String())
+	if len(metric) != metricCount {
+		for _, m := range metric[metricCount:] {
+			fmt.Printf("Update: %q\n", m.String())
+		}
+		metricCount = len(metric)
 	}
 
-	for _, s := range tasks {
-		fmt.Println(s.String())
+	if len(tasks) != tasksCount {
+		for _, m := range tasks[tasksCount:] {
+			fmt.Printf("Update: %q\n", m.String())
+		}
+		tasksCount = len(tasks)
 	}
-
-	fmt.Println("\n\n\n")
+	fmt.Println("Checking done")
 }
--- server/internal/service/service..go
+++ server/internal/service/service..go
@@ -11,27 +11,18 @@ import (
 	"github.com/vv-sam/otus-project/server/internal/model/task"
 )
 
-func GenerateStructs() []fmt.Stringer {
-	// количество от 5 до 15
-	c := rand.Uint32()%10 + 5
+func GenerateStruct(ch chan fmt.Stringer) {
+	// рандомная моделька
+	t := rand.Uint32() % 4
 
-	r := make([]fmt.Stringer, 0, c)
-
-	for range c {
-		// рандомная моделька
-		t := rand.Uint32() % 4
-
-		switch t {
-		case 0:
-			r = append(r, &agent.Info{AgentId: uuid.New(), Status: 0})
-		case 1:
-			r = append(r, &configuration.Minecraft{ServerName: "Otus server", MaxPlayers: uint(c)})
-		case 2:
-			r = append(r, &metrics.HostMetrics{CpuUsage: float32(t) / 8, RamAvailable: 1_000_000, RamTotal: 3_000_000})
-		case 3:
-			r = append(r, &task.Task{Id: uuid.New(), Status: task.STATUS_QUEUED, Type: "test"})
-		}
+	switch t {
+	case 0:
+		ch <- &agent.Info{AgentId: uuid.New(), Status: 0}
+	case 1:
+		ch <- &configuration.Minecraft{ServerName: "Otus server", MaxPlayers: 64}
+	case 2:
+		ch <- &metrics.HostMetrics{CpuUsage: float32(t) / 8, RamAvailable: 1_000_000, RamTotal: 3_000_000}
+	case 3:
+		ch <- &task.Task{Id: uuid.New(), Status: task.STATUS_QUEUED, Type: "test"}
 	}
-
-	return r
 }
